fix precision issue

splines.py
@@ -209,8 +209,9 @@ class AxisSimplestSpline(AbstractSpline, torch.nn.Module):
         # raw is (B, H, W)
         # ys is (B, knots)
         # add the two extra knots 0 and 1
+        eps = 1e-4
         ys = torch.cat([torch.ones_like(ys[:, :1])*xsmin, ys, torch.ones_like(ys[:, :1])*xsmax], dim=1)  # (B, N+2)
-        xs = torch.linspace(0, 1, self.n_knots+2, device=ys.device)[None] * (xsmax - xsmin) + xsmin
+        xs = torch.linspace(0, 1, self.n_knots+2, device=ys.device)[None] * ((xsmax + eps) - xsmin) + xsmin
         # xs = torch.linspace(xsmin, xsmax, self.n_knots+2, requires_grad=True)[None].to(ys.device)  # (1, N+2)
         slopes = torch.diff(ys) / (xs[:, 1] - xs[:, 0])  # (B, N+1)
         out = torch.ones_like(raw) * 99  # placeholder