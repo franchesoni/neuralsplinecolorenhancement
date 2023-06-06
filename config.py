d978bb9b424210598b0b2b108ded429579cf561e
franchesoni <marchesoniacland@gmail.com>
2023-06-06 11:53:19 +0200

    make general

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import torch
 
-DATASET_DIR = '/home/maxdunitz/Desktop/mlbriefs2/workdir/neural_spline_enhancement/C'#dataset/C/'
+# DATASET_DIR = '/home/maxdunitz/Desktop/mlbriefs2/workdir/neural_spline_enhancement/C'
+DATASET_DIR = 'dataset/C/'
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
